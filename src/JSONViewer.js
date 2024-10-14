606831d1625b41f690790e32f11537cf8136ccc5
Rajasekar M <rajasekar.1999@gmail.com>
2024-10-15 02:29:13 +0530

    Nice Changes

---
+++ src/JSONViewer.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import { Container, Typography } from '@mui/material';
+import ReactJson from 'react-json-view';
+import axios from 'axios';
+import dummyData from './dummyData'; // Import dummy data
+
+function JSONViewer() {
+    const [accountData, setAccountData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const accountId = urlParams.get('accountId');
+  
+      const apiEndpoint = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://socrat-iq.onrender.com';
+  
+      const authenticateAndFetchData = async () => {
+        try {
+          const response = await axios.post(`${apiEndpoint}/salesforce/auth`, { account_id: accountId });
+          console.log('Account Data:', response.data);
+          setAccountData(response.data); // Set account data
+        } catch (error) {
+          console.error('Error fetching Salesforce data:', error);
+          setError(error); // Set error state
+        } finally {
+          setIsLoading(false); // Set loading to false after fetching
+        }
+      };
+  
+      authenticateAndFetchData();
+    }, []);
+  
+    const [expandedPanels, setExpandedPanels] = React.useState({
+      open: false,
+      inProgress: false,
+      closed: false,
+    });
+  
+    const handleToggleAll = () => {
+      const expandAll = !expandedPanels.open && !expandedPanels.inProgress && !expandedPanels.closed;
+      setExpandedPanels({
+        open: expandAll,
+        inProgress: expandAll,
+        closed: expandAll,
+      });
+    };
+  
+    const handleAccordionChange = (panel) => (event, isExpanded) => {
+      setExpandedPanels((prevState) => ({
+        ...prevState,
+        [panel]: isExpanded,
+      }));
+    };
+  
+    // Determine which data to use
+    const data = accountData && accountData.Opportunities && accountData.Opportunities.length > 0
+      ? accountData.Opportunities
+      : dummyData;
+  
+    if (isLoading) return <div>Loading...</div>;
+  
+    if (error) return <div>Error: {error}</div>;
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Account Data JSON
+      </Typography>
+      {data ? <ReactJson
+        src={data}
+        theme="summerfruit:inverted"
+        collapsed={2}
+        enableClipboard={true}
+        displayDataTypes={false}
+        displayObjectSize={false}
+      /> : <p>Loading...</p>}
+    </Container>
+  );
+}
+
+export default JSONViewer;
